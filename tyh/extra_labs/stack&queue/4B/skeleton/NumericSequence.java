878b6cfcfa760608c875eb7717de1293b7ea27ff
Tan Yuanhong <leotan.yh@gmail.com>
2019-03-19 13:07:10 +0800

    rename and add new stuff

---
+++ tyh/extra_labs/stack&queue/4B/skeleton/NumericSequence.java
@@ -0,0 +1,24 @@
+/*
+ * CS1020 (AY2014/5 Sem2)
+ * Sit-in Lab 04 B
+ * Author      : 
+ * Matric no.  : 
+ * Lab group   : 
+ * Description :
+ *      ???
+ *      ???
+ */
+
+import java.util.Scanner;
+
+public class NumericSequence {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		NumericSequence ns = new NumericSequence();
+		ns.find(sc);
+    }
+
+    
+}
+
