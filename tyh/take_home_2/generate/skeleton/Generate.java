6ce74eebf55714bdd995f78f9ce9a19410544b65
Tan Yuanhong <leotan.yh@gmail.com>
2019-03-26 14:47:55 +0800

    update

---
+++ tyh/take_home_2/generate/skeleton/Generate.java
@@ -0,0 +1,18 @@
+/**
+ * Name         :
+ * Matric. No   :
+ * PLab Acct.   :
+ */
+
+import java.util.*;
+
+public class Generate {
+	private void run() {
+		//implement your "main" method here
+	}
+
+	public static void main(String[] args) {
+		Generate newGenerate = new Generate();
+		newGenerate.run();
+	}
+}
